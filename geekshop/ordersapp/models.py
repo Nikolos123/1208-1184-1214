c94f452051182dc0bfec3bf860af8248d4bd19db
Nikolos123 <Isponec2>
2021-10-19 21:27:52 +0300

    preparation ordersapp

--- geekshop/ordersapp/models.py
+++ geekshop/ordersapp/models.py
@@ -23,7 +23,7 @@ class Order(models.Model):
 
     )
 
-    user = models.ForeignKey(settings.AUTH_USER_MODE,on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     created = models.DateTimeField(verbose_name='создан',auto_now_add=True)
     update = models.DateTimeField(verbose_name='обновлен', auto_now=True)
     status = models.CharField(choices=ORDER_STATUS_CHOICES,verbose_name='статус',max_length=3,default=FORMING)
--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -3,9 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 
+from ordersapp.models import Order
+
 
 class OrderList(ListView):
-    pass
+    model = Order
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
 
 class OrderCreate(CreateView):
     pass
