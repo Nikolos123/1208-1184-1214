77538619f5aac99abadac274d273dbc52c748444
Nikolos123 <Isponec2>
2021-10-29 22:13:02 +0300

    added static for admin and tool_bar

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -45,14 +45,15 @@ INSTALLED_APPS = [
     'social_django',
     'ordersapp',
     'debug_toolbar',
-    'template_profiler_panel'
+    'template_profiler_panel',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -87,20 +88,20 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'geekshop',
-        'USER': 'postgres',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'geekshop',
+#         'USER': 'postgres',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -138,8 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = (BASE_DIR / 'static',)
+# STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 
 # Default primary key field type
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
-import os,json
+import os, json
 
 from mainapp.models import Product, ProductCategory
-from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 MODULE_DIR = os.path.dirname(__file__)
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'mainapp/index.html')
 
-def products(request,category_id=None,page_id=1):
-    # file_path = os.path.join(MODULE_DIR,'fixtures/goods.json')
-    products = Product.objects.filter(category_id=category_id) if category_id !=None else Product.objects.all()
 
-    paginator = Paginator(products,per_page=3)
+def products(request, category_id=None, page_id=1):
+    # file_path = os.path.join(MODULE_DIR,'fixtures/goods.json')
+    products = Product.objects.filter(category_id=category_id).select_related('category') \
+        if category_id != None else Product.objects.all().select_related('category')
+
+    paginator = Paginator(products, per_page=3)
     try:
         products_paginator = paginator.page(page_id)
     except PageNotAnInteger:
@@ -27,6 +30,6 @@ def products(request,category_id=None,page_id=1):
         'title': 'Катлог',
         'categorys': ProductCategory.objects.all(),
         'products': products_paginator
-        }
+    }
     # context.update({'products':products_paginator})
-    return render(request, 'mainapp/products.html',context)
+    return render(request, 'mainapp/products.html', context)
